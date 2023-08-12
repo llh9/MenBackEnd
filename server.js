0c118daa97cb32b8b89916f08c24d41060d86dfb
llh9 <llh9@yahoo.com>
2023-08-12 11:07:58 -0400

    commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('./config/db');
 
 const app = require('express')();
+var cors = require('cors')  //use this
+
 const port = process.env.PORT;
 
 const UserRouter = require('./api/User');
  
 //For accepting post form data
 const bodyParser = require('express').json;
+app.use(cors()) 
 app.use(bodyParser());
 
 app.use('/user', UserRouter);
