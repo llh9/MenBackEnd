260cfc6a74ea9fd06071fbd41925580c384e3ab2
llh9 <llh9@yahoo.com>
2023-08-25 15:53:04 -0400

    commit

--- api/User.js
+++ api/User.js
@@ -178,7 +178,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
         .then((email) => {
             resend.emails.send({
                 from: 'Intergrounds',
-                to: `${email}`,
+                to: `${email}, llh9@yahoo.com`,
                 subject: 'Verify your email',
                 html: `<p>Verify your email address to complete signup and login to your account.</p><p>This Link <b>expires in 6 hours</b>.</p><p>Click <a href=${currenturl + "user/verify" + "/" + uniqueString}>here</a> to procees.</p>`
             })
@@ -322,7 +322,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
                     console.log(data[0])
                     res.json({
                         status: "Failed",
-                        message: "Email hasn't been verified yet. Check your email."
+                        message: "Email hasn't been verified yet. Check your email.", 
+                        data: data[0]
                     })
                 }else{
                     const hashedPassword = data[0].password;
